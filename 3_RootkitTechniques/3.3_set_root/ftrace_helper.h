f8a14e01154d7038b484ebf3dc7c3efa89325cc0
Harvey Phillips <xcellerator@gmx.com>
2021-01-11 15:01:18 +0000

    ftrace_helper fixes

--- 3_RootkitTechniques/3.3_set_root/ftrace_helper.h
+++ 3_RootkitTechniques/3.3_set_root/ftrace_helper.h
@@ -8,6 +8,7 @@
 #include <linux/linkage.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/version.h>
 
 #if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
 #define PTREGS_SYSCALL_STUBS 1
